2f28d87001f388370f1dc8042c51f8d040542530
matsumoto mika <mk.mtmt48@gmail.com>
2025-04-18 15:27:48 +0900

    【課題２】⑤ コントローラ編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,24 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path, notice: "投稿完了！"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.requier(:post).permit(:content)
   end
 end
